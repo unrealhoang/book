bb6f443f8d09baaec013ed685d1c2a5edb6fe8c6
Giang Nguyen <nguyengiangdev@gmail.com>
2016-11-29 14:34:01 +0700

    Test remote deploy

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -10,13 +10,11 @@ git init
 git config user.name "Giang Nguyen"
 git config user.email "nguyengiangdev@gmail.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/rust-vietnam/rust-programming-language-book.git"
-git remote -v
-git fetch upstream
-git reset upstream/gh-pages
+git remote add deploy "https://$GH_TOKEN@github.com/rust-vietnam/rust-programming-language-book.git"
+git fetch deploy 
 
 touch .
 
 git add -A .
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages > /dev/null 2>&1
+git push -q deploy HEAD:gh-pages > /dev/null 2>&1
