3afd71e337868a379d4ef1ed62446172082c37b3
Giang Nguyen <nguyengiangdev@gmail.com>
2016-11-29 14:26:50 +0700

    Add command test

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -11,6 +11,7 @@ git config user.name "Giang Nguyen"
 git config user.email "nguyengiangdev@gmail.com"
 
 git remote add upstream "https://$GH_TOKEN@github.com/rust-vietnam/rust-programming-language-book.git"
+git remote -v
 git fetch upstream
 git reset upstream/gh-pages
 
