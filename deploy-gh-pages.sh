069bfae9f8858b4f8ed36b2e03ecbaa96038109b
Giang Nguyen <nguyengiangdev@gmail.com>
2016-11-29 14:16:59 +0700

    Add script automatic deploy

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd book
+
+git init
+git config user.name "Giang Nguyen"
+git config user.email "nguyengiangdev@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/rust-vietnam/rust-programming-language-book.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages > /dev/null 2>&1
